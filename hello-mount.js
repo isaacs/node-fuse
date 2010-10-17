7dfb6c755ecd7ca6a1b863ddda5c5558760a6a63
isaacs <i@izs.me>
2010-10-16 19:59:19 -0700

    better test

--- hello-mount.js
+++ hello-mount.js
@@ -1,10 +1,17 @@
 
 var mp = process.argv[2]
   , args = process.argv.slice(3)
+  , child_process = require("child_process")
 if (!mp) throw new Error("usage: node "+process.argv[1]+" <mountpoint> [args]")
 
 require("./build/default/hello-ll").mount(mp, args, function (er) {
-  console.error(er && er.stack || "ok")
+  console.error(er && er.stack || "mounted and unmounted ok")
 })
 
-console.log("if you see this before it ends, then async works.")
+// kludge, need to make it fire the cb on init rather than unmount
+setTimeout(function () {
+  require("fs").readFile(require("path").join(mp, "hello"), function (er, hw) {
+    if (er) throw er
+    console.log(hw.toString())
+  })
+}, 200)
